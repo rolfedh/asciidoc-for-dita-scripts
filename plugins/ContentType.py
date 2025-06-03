306bd275ac3dad5eaa8bcc881622970274dd08ba
Roger Heslop <rheslop@redhat.com>
2025-06-03 16:31:21 -0500

    ContentType plugin check now matches the Vale plugin regular expression check

--- plugins/ContentType.py
+++ plugins/ContentType.py
@@ -7,6 +7,7 @@ This plugin adds a :_mod-docs-content-type: label in .adoc files where those are
 __description__ = "Add a :_mod-docs-content-type: label in .adoc files where those are missing, based on filename."
 
 import os
+import re
 
 class highlighter(object):
     def __init__(self, text):
@@ -18,7 +19,7 @@ class highlighter(object):
     def highlight(self):                                                                                                                                                                                    
         return('\033[0;36m' + self.text + '\033[0m')
 
-def editor(filepath, label1, label2):
+def editor(filepath, label):
     # If the label does not exist, the editor adds it.
     try:
         with open(filepath, 'r+', encoding='utf-8') as f:
@@ -26,16 +27,16 @@ def editor(filepath, label1, label2):
             label_exists = False
 
             for line_content in lines:
-                if label1.strip() == line_content.strip() or label2.strip() == line_content.strip():
+                if re.search('(?:^|[\n\r]):_(?:mod-docs-content|content|module)-type:[ \t]+\S', line_content.strip()):
                     label_exists = True
                     break
 
             if label_exists:
                 print(f"Skipping {filepath}, label already present")
             else:
-                print(highlighter(f"Editing: Adding {label1} to {filepath}").bold())
+                print(highlighter(f"Editing: Adding content type to {filepath}").bold())
                 f.seek(0, 0)
-                f.write(label1 + "\n")
+                f.write(f":_mod-docs-content-type: {label}" + "\n")
 
                 # Adds content back
                 for line in lines:
@@ -54,24 +55,19 @@ def tree_walker(directory):
             if file.startswith(".") or not file.endswith(".adoc"):
                 continue
             filepath = os.path.join(root, file)
-            label1 = None
-            label2 = None
+            label = None
 
             if file.startswith("assembly_"):
-                label1 = ":_module-type: ASSEMBLY" 
-                label2 = ":_mod-docs-content-type: ASSEMBLY"
+                label = "ASSEMBLY" 
             elif file.startswith("con_"):
                 label1 = ":_module-type: CONCEPT"
-                label2 = ":_mod-docs-content-type: CONCEPT"
             elif file.startswith("proc_"):
                 label1 = ":_module-type: PROCEDURE"
-                label2 = ":_mod-docs-content-type: PROCEDURE"
             elif file.startswith("ref_"):
                 label1 = ":_module-type: REFERENCE"
-                label2 = ":_mod-docs-content-type: REFERENCE"
 
-            if label1 or label2:
-                editor(filepath, label1, label2)
+            if label
+                editor(filepath, label)
             else:
                 pass
 
